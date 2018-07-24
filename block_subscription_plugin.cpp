3650f2633fc72a22376d7e910431e9ce1e7a6e96
kirillsurkov <surkov.k@mywish.io>
2018-07-24 18:06:54 +0300

    ws

--- block_subscription_plugin.cpp
+++ block_subscription_plugin.cpp
@@ -4,23 +4,48 @@
 #include <eosio/chain/block.hpp>
 #include <boost/signals2/connection.hpp>
 #include <fc/io/json.hpp>
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
 
 namespace eosio {
 	static appbase::abstract_plugin& _block_subscription_plugin = app().register_plugin<block_subscription_plugin>();
 
 	class block_subscription_plugin_impl {
+	private:
+		websocketpp::server<websocketpp::config::asio> server;
+		std::set<websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>> connections;
+		fc::optional<abi_serializer> (*resolver)(const account_name&);
+
 	public:
 		fc::optional<boost::signals2::scoped_connection> accepted_block_connection;
 		chain_plugin& chain_plugin_ref;
 
 		block_subscription_plugin_impl(chain_plugin& chain_plugin_ref) :
+			resolver([](const account_name& name) {
+				return fc::optional<abi_serializer>();
+			}),
 			chain_plugin_ref(chain_plugin_ref)
-		{}
+		{
+			this->server.clear_access_channels(websocketpp::log::alevel::all);
+			this->server.init_asio(&app().get_io_service());
+			this->server.set_reuse_addr(true);
+			this->server.set_open_handler([this](websocketpp::connection_hdl hdl) {
+				this->connections.insert(hdl);
+			});
+			this->server.set_close_handler([this](websocketpp::connection_hdl hdl) {
+				this->connections.erase(hdl);
+			});
+			this->server.listen(8886); // TODO: load port from config
+			this->server.start_accept();
+		}
 
 		void on_block(const chain::signed_block_ptr& block) {
+			this->chain_plugin_ref.get_abi_serializer_max_time();
 			fc::variant output;
-			fc::to_variant(static_cast<const chain::signed_block&>(*block), output);
-			ilog(fc::json::to_string(output));
+			abi_serializer::to_variant(static_cast<const chain::signed_block&>(*block), output, this->resolver, this->chain_plugin_ref.get_abi_serializer_max_time());
+			for (auto connection : this->connections) {
+				this->server.send(connection, fc::json::to_string(output), websocketpp::frame::opcode::text);
+			}
 		}
 	};
 
@@ -29,12 +54,12 @@ namespace eosio {
 			app().get_plugin<chain_plugin>()
 		))
 	{}
+
 	block_subscription_plugin::~block_subscription_plugin(){}
 
 	void block_subscription_plugin::set_program_options(options_description&, options_description& cfg) {}
 
-	void block_subscription_plugin::plugin_initialize(const variables_map& options) {
-	}
+	void block_subscription_plugin::plugin_initialize(const variables_map& options) {}
 
 	void block_subscription_plugin::plugin_startup() {
 		ilog("starting block_subscription_plugin");
